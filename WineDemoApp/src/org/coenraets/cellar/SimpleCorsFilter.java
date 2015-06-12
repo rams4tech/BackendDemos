3881fddeaee9788ae394b76b25ef75e824138181
ramesh.sunkari <ramesh.sunkari@kony.com>
2015-06-12 22:04:23 +0530

    cors filter changes

--- WineDemoApp/src/org/coenraets/cellar/SimpleCorsFilter.java
+++ WineDemoApp/src/org/coenraets/cellar/SimpleCorsFilter.java
@@ -24,7 +24,7 @@ public class SimpleCorsFilter  implements ContainerResponseFilter {
  
         ResponseBuilder resp = Response.fromResponse(contResp.getResponse());
         resp.header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
  
         String reqHead = req.getHeaderValue("Access-Control-Request-Headers");
  
